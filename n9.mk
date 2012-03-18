da20e630163a45a970d81f6abf4a60ce6b0f3ae0
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2012-03-19 00:41:10 +0400

    Fix build problem (PRODUCT_COPY_FILES)

--- n9.mk
+++ n9.mk
@@ -2,18 +2,21 @@
 # It includes the base Android platform. If you need Google-specific features,
 # you should derive from generic_with_google.mk
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-DEVICE_PACKAGE_OVERLAYS := vendor/nitdroid/n9/overlay
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # This is the list of apps included in the generic AOSP build
-PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES := \
     Camera \
     Development \
     SpareParts \
     SoundRecorder \
     VoiceDialer
 ##
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+DEVICE_PACKAGE_OVERLAYS := vendor/nitdroid/n9/overlay
 
 # Overrides
 PRODUCT_BRAND := nokia
