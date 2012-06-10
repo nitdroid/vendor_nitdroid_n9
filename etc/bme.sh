2673c0fcc234ba94c0021f0ded6a24f62f97cf00
Alexey Roslyakov <alexey.roslyakov@newsycat.com>
2012-06-10 13:32:56 +0400

    Don't strace BME anymore

--- etc/bme.sh
+++ etc/bme.sh
@@ -3,6 +3,8 @@
 HARMATTAN_ROOT=/mnt/initfs
 
 # we need to wait a little for fakedsme
-sleep 1
+sleep 2
 
-exec strace chroot ${HARMATTAN_ROOT} /usr/sbin/bme_RX-71 -u -v 5 -c /usr/lib/hwi/hw/rx71.so
+rm ${HARMATTAN_ROOT}/tmp/.bme* 2> /dev/null
+
+chroot ${HARMATTAN_ROOT} /usr/sbin/bme_RX-71 -l stdout -v 5 --nodsme -c /usr/lib/hwi/hw/rx71.so
